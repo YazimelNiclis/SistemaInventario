ef0458181bc2cbc29f2d13623e95a389c3364441
YazimelNiclis <yazimel_niclis@hotmail.com>
2023-12-06 11:40:46 -0300

    Patron Repositorio

---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IRepositorio<T> where T : class
+    {
+        //con task le digo que sea asincrono
+        Task<T> Obtener(int id);
+        Task<IEnumerable<T>> ObtenerTodos(
+            Expression<Func<T, bool>> filtro = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string incluirPropiedades = null,
+            bool isTracking = true
+            );
+        Task<T> ObtenerPrimero(
+            Expression<Func<T, bool>> filtro = null,
+            string incluirPropiedades = null,
+            bool isTracking = true
+            );
+
+        Task Agregar(T entidad);
+
+        //No pueden ser asincronos. Por que?
+        void Remover(T entidad);
+
+
+        void RemoverRango(IEnumerable<T> entidad);
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
+{
+    //IDisposable te permite deshacerte de cualquier recuros que haya obtenido el sistema y liberar recursos
+    public interface IUnidadTrabajo : IDisposable
+    {
+        IBodegaRepositorio Bodega { get; }
+
+        Task Guardar();
+    }
+}
